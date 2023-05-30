05ece3cf62ef6d3086148b9d02b348e176f90120
Vivian <bergerv.tin22@student.dhbw-heidenheim.de>
2023-05-30 14:32:50 +0200

    Connected values in line chart with lines and added axis label.

--- src/line_chart.rs
+++ src/line_chart.rs
@@ -1,6 +1,6 @@
 use leptos::*;
-use nalgebra::{Matrix2, Point2, Vector2};
-// use leptos::ev::Event;
+use leptos::svg::Text;
+use nalgebra::{Matrix2, point, Point2, Vector2};
 
 #[component]
 pub fn LineChart(cx: Scope) -> impl IntoView {
@@ -11,7 +11,7 @@ pub fn LineChart(cx: Scope) -> impl IntoView {
         (5.0, 1.0),
         (6.4, 5.0),
         (10.0, 3.8),
-        (16.3, 5.3)
+        (16.9, 5.3)
     ];
     let max_x = data.iter().map(|(x, _)| *x).reduce(f32::max).unwrap();
     let max_y = data.iter().map(|(_, y)| *y).reduce(f32::max).unwrap();
@@ -35,15 +35,50 @@ pub fn LineChart(cx: Scope) -> impl IntoView {
 
     let points = data.iter().map(|(x, y)| {
        let point = Point2::new(*x, *y);
-        (scale * point) + translation;
+        (scale * point) + translation
     }).collect::<Vec<Point2<f32>>>();
 
     let circles = points.iter().zip(data).map(|(point, (x, y))| {
         view! {cx,
-            <circle cx=point.x cy=point.y r="5" fill="red"></circle>
-            <text x=point.x y=point.y>{format!("{:.2},{:.2} ~ {:.2},{:.2}", point.x, point.y, x, y)}</text>
+            <circle cx=point.x cy=point.y r="3" fill="red"></circle>
+            // <text x=point.x y=point.y>{format!("{:.2},{:.2} ~ {:.2},{:.2}", point.x, point.y, x, y)}</text>
         }
     }).collect::<Vec<_>>();
+
+    let lines = points.iter().take(points.len() - 1).zip(points.iter().skip(1)).map(|(start_point, end_point)| {
+        view! {cx,
+            <line x1=start_point.x y1=start_point.y x2=end_point.x y2=end_point.y stroke="black"></line>
+        }
+    }).collect::<Vec<_>>();
+
+
+    let label_x =
+        (((min_x * 10.0) as i32)..((max_x * 10.0) as i32))
+            .step_by(1 * 10)
+            .map(|value| {
+                let text_y = height - 0.5 * padding;
+                let point = Point2::new(value as f32 / 10.0, 0.0);
+                let position = (scale * point) + translation;
+                view! {cx,
+                    <line x1=position.x y1=(text_y - 20.0) x2=position.x y2=(text_y - 30.0) stroke="black"></line>
+                    <text x=position.x y=text_y class="small">{value / 10}</text>
+                }
+            })
+            .collect::<Vec<_>>();
+
+    let label_y =
+        (((min_y * 10.0) as i32)..((max_y * 10.0) as i32))
+            .step_by(1 * 10)
+            .map(|value| {
+                let text_x = 0.5 * padding;
+                let point = Point2::new(0.0, value as f32 / 10.0);
+                let position = (scale * point) + translation;
+                view! {cx,
+                    <line y1=position.y x1=(padding - 5.0) y2=position.y x2=(padding + 5.0) stroke="black"></line>
+                    <text y=position.y x=text_x class="small">{value / 10}</text>
+                }
+            })
+            .collect::<Vec<_>>();
 
     view! {cx,
         <svg width=width height=height>
@@ -52,12 +87,11 @@ pub fn LineChart(cx: Scope) -> impl IntoView {
             <text stroke="black">
                 <textPath href="#YAxis">"Wasserstand"</textPath>
             </text>
+            {lines}
             {circles}
-            <text x="0" y="20" class="small">"Datum 0"</text>
-            <text x="100" y="20" class="small">"Datum 1"</text>
-            <text x="200" y="20" class="small">"Datum 2"</text>
+            {label_x}
+            {label_y}
             <rect width=move || width - 2.0 * padding height= move || height - 2.0 * padding x=padding y=padding fill="none" stroke="black"></rect>
-            <text x="300" y="20" class="small">"Datum 3"</text>
         </svg>
     }
 }
